c10fec705cf83487807dd6bd39d342f99cd674a5
SeokGeunHo <kimc6555@gmail.com>
2025-05-23 12:25:13 +0900

    5단계 명언 목록 조회 기능 구현

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,13 +1,16 @@
 package  com.back;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int count = 0;
+        int id = 0;
+
         System.out.println("== 명언 앱 ==");
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<WiseSaying> wiseSayingArrayList = new ArrayList<>();
 
         while ( true ) {
             System.out.print("명령) ");
@@ -18,11 +21,21 @@ public class Main {
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String wiseSayingContent = scanner.nextLine().trim();
+
                 System.out.print("작가 : ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
 
-                count++;
-                System.out.println(count + "번 명언이 등록되었습니다.");
+                id++;
+                wiseSayingArrayList.add(new WiseSaying(id, wiseSayingContent, wiseSayingAuthor));
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for (int i = wiseSayingArrayList.size() - 1; i >= 0; i--) {
+                    WiseSaying wiseSaying = wiseSayingArrayList.get(i);
+                    System.out.println(wiseSaying);
+                }
             }
         }
 
---
+++ src/main/java/com/back/WiseSaying.java
@@ -0,0 +1,18 @@
+package com.back;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "%d / %s / %s".formatted(id, content, author);
+    }
+}
